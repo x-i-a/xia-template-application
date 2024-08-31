55bd9a070e4be7644b78ed60bff6a3924cd0288a
X-I-A <develop@x-i-a.com>
2024-08-31 22:04:41 +0200

    feat: adding current env

--- iac/environments/base/locals.tf
+++ iac/environments/base/locals.tf
@@ -2,4 +2,5 @@ locals {
   landscape = yamldecode(file(var.landscape_file))
   modules = yamldecode(file(var.modules_file))
   environment_dict = lookup(local.landscape, "environments", {})
+  current_env = basename(abspath(path.root))
 }
